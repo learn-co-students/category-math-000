977b28945ab481e37d7003c80560b7c9be8359ab
Raskovan <alexey.katalkin@gmail.com>
2016-12-06 22:09:13 -0500

    Numbers

---
+++ CategoryMath/NSNumber+FISMath.h
@@ -0,0 +1,18 @@
+//
+//  NSNumber+FISMath.h
+//  CategoryMath
+//
+//  Created by Katala on 12/6/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (FISMath)
+
+-(NSNumber *) numberByAddingNumber: (NSNumber *) number;
+-(NSNumber *) numberBySubtractingNumber: (NSNumber *) number;
+-(NSNumber *) numberByMultiplyingByNumber: (NSNumber *) number;
+-(NSNumber *) numberByDividingByNumber: (NSNumber *) number;
+
+@end
